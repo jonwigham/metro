0cd7040fb763154602bf19798284de6a99884665
Jon Wigham <jon.wigham@gmail.com>
2012-09-08 21:52:37 +0100

    A couple of tweaks to the responsive styles

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo("charset"); ?>" />
+	<meta name="viewport" content="width=device-width" />
+
 	<title>
 	<?php
 		global $page, $paged;
