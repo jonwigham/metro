eef3a9c63090c8b682f3416af0ba890aadebaa04
Jon Wigham <jon.wigham@gmail.com>
2012-09-11 20:46:06 +0100

    #1 Post navigation links added

--- loop.php
+++ loop.php
@@ -72,3 +72,11 @@
 	<?php comments_template("", true); ?>
 
 <?php endwhile; ?>
+
+<?php if ($wp_query->max_num_pages > 1): ?>
+	<div id="nav-below" class="navigation">
+		<div class="nav nav-previous"><?php next_posts_link(__('<span class="meta-nav">&laquo;</span> Older posts')); ?></div>
+		<div class="nav nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&raquo;</span>')); ?></div>
+		<div class="clear"></div>
+	</div>
+<?php endif; ?>
